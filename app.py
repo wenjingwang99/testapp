80fc3994ae29d2ae64be664e0b866ef78022d274
wenjingwang99 <Githubsuccess66>
2022-02-03 13:24:06 -0500

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Dash(__name__)
 server = app.server
 app.layout = html.Div([
     github_info_header(),
-    html.Img(src="assets/sea_otter_new.jpg")
+    html.Img(src="assets/sea_otter.jpg")
 ])
 
 if __name__ == '__main__':
