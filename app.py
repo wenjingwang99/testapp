156df52bc8b97f713075fafed2e13eb73044092f
wenjingwang99 <Githubsuccess66>
2022-02-04 00:34:53 -0500

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Dash(__name__)
 server = app.server
 app.layout = html.Div([
     github_info_header(),
-    html.Img(src="assets/jojo.jpg")
+    html.Img(src="assets/mista.jpg")
 ])
 
 if __name__ == '__main__':
